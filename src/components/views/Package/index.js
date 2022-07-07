ffcef86f1bb24b1c8ef11f5299b203c16c59ea63
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-07 15:40:26 -0300

    adjust

--- src/components/views/Package/index.js
+++ src/components/views/Package/index.js
@@ -7,14 +7,6 @@ const Package = () => {
 
     return(
         <div className="package wd-cont">
-            <div className="sec-1">
-                {/* <img src={OwlCoin} alt="Coruja com moedas"/> */}
-                <div className="info">
-                    <h2>Compre pacotes e ganhe BNZ's!</h2>
-                    <p>Nossa Moeda Virtual Bonuz. Adquira um pacote, ganhe BNZ’s e troque por Serviços! </p>
-                    <p>Seu saldo BNZ's não expira!</p>
-                </div>               
-            </div>
             <div className="sec-2">
                 <h2>Nossos Pacotes</h2>
                 <p>Adquira uns dos pacotes Bonuz, ganhe créditos BNZ's (nossa moeda virtual) e troque por serviços. </p>
